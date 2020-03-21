df74ba95849075ce2390c984d07b7fad862b3f6f
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-03-20 19:21:05 -0700

    changed usersInpute to a string value

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //takes users input
 $("#submitCity").on("click",function(event){
     event.preventDefault();
-    var usersInput = $("#usersInput").val().trim();
+    var usersInput = JSON.stringify($("#usersInput").val().trim());
     console.log(usersInput)
 });
+
+
