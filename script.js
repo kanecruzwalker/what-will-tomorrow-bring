764fbf19ffb138af3c21822e1c4ed45ed09ffbc1
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-03-20 23:51:35 -0700

    added . click to mainDisplay. Added header classes to header tag

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+//adding current date to document
+var date =JSON.stringify(moment().format("MM-DD-YYYY"));
+$("#dateHeader").text("Today's Forecast " + " " + date);
+
 //takes users input
 $("#submitCity").on("click",function(event){
     event.preventDefault();
     var usersInput = JSON.stringify($("#usersInput").val().trim());
     console.log(usersInput)
+    $("#mainDisplay").text( JSON.parse(usersInput) + " " + JSON.parse(date));
 });
-
 
