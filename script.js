b0262f5be5936f3cdf0ab57620f39de9731d5ed4
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-03-21 01:19:54 -0700

    attempted to use ajax to access api values. Needs completion

--- script.js
+++ script.js
@@ -4,19 +4,62 @@ $("#dateHeader").text("Today's Forecast " + " " + date);
 
 //takes users input
 $("#submitCity").on("click",function(event){
-    event.preventDefault();
     var usersInput = JSON.stringify($("#usersInput").val().trim());
+    event.preventDefault();
     console.log(usersInput)
     $("#mainDisplay").text( JSON.parse(usersInput) + " " + JSON.parse(date));
 
     $("#firstSearch").text( JSON.parse(usersInput));
 });
 
+// function displayWeatherInfo() {
+//     var weatherData = $(this).attr("data-name");
+//     var queryURL = "api.openweathermap.org/data/2.5/weather?q="+ weatherData + "&appid=daf08b427b26f11b51a8b3849391acfc"}
+
+//     ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+    
+//     });
+// }
+
+
+
+
+
+
+
+
+
+// function displayWeather() {
+//     var temperature = $(this).attr("data-name");
+//     var weatherQueryUrl = "api.openweathermap.org/data/2.5/weather?q="+ usersInput + "&appid=daf08b427b26f11b51a8b3849391acfc"
+//     console.log(weatherQueryUrl);
+    
+//     $.ajax({
+//         url: weatherQueryUrl,
+//         method: "GET"
+//     }).then(function(result){ 
+//         console.log(result);
+//     });
+//     console.log(result);
+// }
+
 //place holder area must be array of sorts 
 // possible function to cycle through searches. 
 // probably needs some looping 
 // $("#firstSearch").on("click", function (event) {
-//     event.preventDefault();
-//     //full assigned usersInput from it and re insert it to search
-
-// });
+    //     event.preventDefault();
+    //     //full assigned usersInput from it and re insert it to search
+    
+    // });
+    
+    // have a list of cities to pull and push from?
+    // based off user input values 
+    // var cities = [
+    // "",
+    // "",
+    // "",
+    // "",
+    // ]
