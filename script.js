77b0a4cdf58ed2a5cd390f1c8509f8d1d8b1eabf
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-03-21 00:08:18 -0700

    attempted creating past searches area. Needs more work. added some pseudo

--- script.js
+++ script.js
@@ -8,5 +8,15 @@ $("#submitCity").on("click",function(event){
     var usersInput = JSON.stringify($("#usersInput").val().trim());
     console.log(usersInput)
     $("#mainDisplay").text( JSON.parse(usersInput) + " " + JSON.parse(date));
+
+    $("#firstSearch").text( JSON.parse(usersInput));
 });
 
+//place holder area must be array of sorts 
+// possible function to cycle through searches. 
+// probably needs some looping 
+// $("#firstSearch").on("click", function (event) {
+//     event.preventDefault();
+//     //full assigned usersInput from it and re insert it to search
+
+// });
